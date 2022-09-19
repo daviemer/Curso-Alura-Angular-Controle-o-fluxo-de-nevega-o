286e8a9c487e73f744a2cf5e2cddc8d04a2127e9
Davi Emer <daviemer2003@hotmail.com>
2022-09-19 12:21:06 -0300

    curso angular

---
+++ gatitobook/src/app/animais/lista-animais/lista-animais.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Animais } from '../animais';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-lista-animais',
+  templateUrl: './lista-animais.component.html',
+  styleUrls: ['./lista-animais.component.css']
+})
+export class ListaAnimaisComponent implements OnInit {
+
+  animais!: Animais;
+
+  constructor(
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((param) =>{
+      this.animais = this.activatedRoute.snapshot.data['animais'];
+    })
+  }
+
+
+}
